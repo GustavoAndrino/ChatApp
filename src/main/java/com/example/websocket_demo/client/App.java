822cb3c4ff903e159ddcbf26112d8df42632522b
Gustavo Andrino <gustavohtandrino@gmail.com>
2024-11-15 12:50:25 -0300

    Username input added

--- src/main/java/com/example/websocket_demo/client/App.java
+++ src/main/java/com/example/websocket_demo/client/App.java
@@ -2,6 +2,7 @@ package com.example.websocket_demo.client;
 
 import java.util.concurrent.ExecutionException;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class App {
@@ -9,9 +10,18 @@ public class App {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+
+					String username = JOptionPane.showInputDialog(null, "Enter Username (Max: 16 Characters): ", 
+							"Chat Application", JOptionPane.QUESTION_MESSAGE);
+					
+					if(username == null || username.isEmpty() || username.length() > 16) {
+						JOptionPane.showMessageDialog(null, "Invalid Username", "error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}				
+				
 				ClientGUI clientGUI = null;
 				try {
-					clientGUI = new ClientGUI("TapTap");
+					clientGUI = new ClientGUI(username);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} catch (ExecutionException e) {
